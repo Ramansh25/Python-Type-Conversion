d377e11d1ce4144cf9cfe9164f247a1a6fedfd6c
Ramansh25 <146805645+Ramansh25@users.noreply.github.com>
2024-08-05 23:41:13 +0530

    Add files via upload

---
+++ PW ASSIN1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Convert an integer to a floating-point number:
+integer_value = 5
+float_value = float(integer_value)
+print(float_value)  
+
+
+# In[2]:
+
+
+# Convert a float to an integer:
+float_value = 5.7
+integer_value = int(float_value)
+print(integer_value)  
+
+
+# In[3]:
+
+
+# Convert an integer to a string:
+integer_value = 10
+string_value = str(integer_value)
+print(string_value)  
+
+
+# In[4]:
+
+
+# Convert a list to a tuple:
+list_value = [1, 2, 3]
+tuple_value = tuple(list_value)
+print(tuple_value)  
+
+
+# In[5]:
+
+
+# Convert a tuple to a list:
+tuple_value = (1, 2, 3)
+list_value = list(tuple_value)
+print(list_value)  
+
+
+# In[6]:
+
+
+# Convert a non-zero number to a boolean:
+non_zero_number = 3
+boolean_value = bool(non_zero_number)
+print(boolean_value)  
+
+
+# In[7]:
+
+
+# Convert a decimal number to a binary:
+decimal_number = 10
+binary_value = bin(decimal_number)
+print(binary_value)  
+
+
+# In[ ]:
+
+
+
+
